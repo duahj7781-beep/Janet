809cf5786b66d3082348db736663643bb1c2c0f2
duahj7781-beep <duahj7781@gmail.com>
2026-09-11 16:36:36 +0300

    Question3

---
+++ .vscode/Question3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int age = 20;
+	float Examinationscore =78.50;
+	float gpa = 3.54;
+	char grade = 'B';
+	
+	printf("Age: %d\n", age);
+	printf("Examinationscore: %.2f\n", Examinationscore);
+	printf("Gpa: %.2f\n", gpa);
+	printf("Grade: %c\n", grade);
+	return 0;
+}
