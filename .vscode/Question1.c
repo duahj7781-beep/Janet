f263f05fdb8302a53fdad3d5e6a2a48c118018d7
duahj7781-beep <duahj7781@gmail.com>
2026-09-11 10:45:31 +0300

    Question1

---
+++ .vscode/Question1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {//Main function 
+
+printf("C Programming Fundamentals\n");
+
+
+
+//Output Statement
+printf("Name,Janet Pomaa Duah\n");
+printf("Student ID,01252656B\n");
+printf("Programme,Biomedical Engineering\n");
+printf("Welcome to C Programming\n");
+	return 0;
+}
