ebdfbf636108a3c2f426a43bd22f382a61035d7c
duahj7781-beep <duahj7781@gmail.com>
2026-09-12 23:20:45 +0300

    Question2

--- .vscode/Question2c.c
+++ .vscode/Question2c.c
@@ -4,7 +4,7 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	//Added semicolons and corrected main() structure
+	// Added semicolons and corrected main() structure
 	printf("Learning C Programming");
 	return 0;
 }
