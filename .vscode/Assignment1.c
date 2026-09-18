eb9300a23faa567913d06a10e017e5ab98e6c7c9
duahj7781-beep <duahj7781@gmail.com>
2026-09-18 22:27:05 +0300

    Assignment1

---
+++ .vscode/Assignment1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// FUNCTION DECLARATION
+void welcome();
+
+int main() {
+	 
+	// FUNCTION CALL
+	welcome();
+	return 0;
+}
+	
+		// FUNCTION DEFINITION
+		void welcome()
+		{
+		printf("Welcome to C Programming\n");
+		printf("Today we are learning about functions\n");
+			
+		}
+		
+		
+	
