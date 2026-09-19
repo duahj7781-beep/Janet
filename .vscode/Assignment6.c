605b2cf09a0e8dfed0ebdb84d6759eeadabfff00
duahj7781-beep <duahj7781@gmail.com>
2026-09-19 23:56:50 +0300

    Assignment6

---
+++ .vscode/Assignment6.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void displayGrade(float score);
+
+
+int main() {
+	
+	float score;
+	
+	printf("Enter your score:");
+	scanf("%f", &score);
+	
+	displayGrade(score);
+	return 0;
+}
+
+void displayGrade(float score)
+{
+	if(score < 0 || score > 100)
+	{
+		printf("Invalid score.\n");
+	}
+	else if (score >=80)
+	{
+		printf("Grade: A\n");
+	}
+	else if (score >=70)
+	{
+		printf("Grade: B\n");
+	}
+	else if (score >=60)
+	{
+		printf("Grade: C\n");
+	}
+	else if (score >=50)
+	{
+		printf("Grade: D\n");
+	}
+	else
+	{
+	printf("Grade: F\n");	
+	}
+	
+}
