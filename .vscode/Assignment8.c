ae0688b497b64cb31f183d72c7053831352d6142
duahj7781-beep <duahj7781@gmail.com>
2026-09-20 00:23:41 +0300

    Asssignment8

---
+++ .vscode/Assignment8.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+void multiplicationTable(int number);
+
+int main() {
+	
+	int number;
+	printf("Enter a number:");
+	scanf("%d", &number);
+	
+	multiplicationTable(number);
+	return 0;
+}
+
+void multiplicationTable(int number)
+{
+	int i;
+	for(i = 1; i <= 12; i++)
+	
+	{
+	printf("%d x %d = %d\n", number, i, number* i);
+	}
+}
