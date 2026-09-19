d759705fe14f18a4c19d3aa2823bb3b40cdb8f7f
duahj7781-beep <duahj7781@gmail.com>
2026-09-20 00:15:29 +0300

    Assignment7

---
+++ .vscode/Assignment7.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+float calculateAverage(float score1, float score2, float score3)
+{
+	return(score1 + score2 + score3) / 3;
+}
+
+
+int main() {
+	float score1, score2, score3, average;
+	
+	printf("Enter first score:");
+	scanf("%f", &score1);
+	
+	printf("Enter second score:");
+	scanf("%f", &score2);
+	
+	printf("Enter third score:");
+	scanf("%f", &score3);
+	
+	average = calculateAverage(score1,  score2, score3);
+	printf("Average Score = %.2f\n", average);
+	return 0;
+}
